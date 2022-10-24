1335e2e0c918aba5056e32d66660dbac12f56771
nagibnur <nagibnur@hotmail.com>
2022-10-24 19:30:24 +0100

    added event to start button

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const answer3 = document.querySelector('#answer3');
 const answer4 = document.querySelector('#answer4');
 const timeEl = document.querySelector('#time');
 const submit_page = document.querySelector('#submit_page');
+
+
+
+start_button.addEventListener('click',  function () {
+    quiz_container.hidden = false;
+    start_button.hidden = true;
+    display_question(current_question);
+    current_question ++;
+    setTime();
+
+});
